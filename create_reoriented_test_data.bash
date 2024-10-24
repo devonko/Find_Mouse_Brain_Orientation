b6979f9b2b6a308228d067ac3d9615fdc8d3bcf5
BJ (Robert) Anderson <floydmitchell@hotmail.com>
2024-10-24 16:59:19 -0400

    Added test data generator.

---
+++ create_reoriented_test_data.bash
@@ -0,0 +1,57 @@
+#! /bin/env bash
+image=$1;
+out_code=$2;
+output_dir=$3;
+if [[ ${image:0:1} != '/' ]];then
+        file_name=$image;
+        folder=$PWD;
+else
+        file_name=${image##*/};
+        folder=${image%/*}
+fi
+
+if [[ ! -d ${output_dir} ]]; then
+        mkdir -m 775 ${output_dir};
+fi
+
+in_code=LAS;
+
+out_codes=();
+RHS=(ALS PRS ARI PLI RAS LPS LAI RPI SAL SPR IAR IPL SRA SLP ILA IRP LSA RSP RIA LIP ASR PSL AIL PIR);
+LHS=(PLS ARS PRI ALI LAS RPS RAI LPI IAL IPR SAR SPL IRA ILP SLA SRP RSA LSP LIA RIP PSR ASL PIL AIR);
+
+# Assume things are all right-handed:
+out_codes=(${RHS[@]}) 
+
+
+if ((0));then
+	input=${tmp_work}${file_name}_down_sampled.nii.gz;
+	ovs_1=$(PrintHeader ${target} 1 | cut -d  'x' -f1);
+	ovs_2=$(PrintHeader ${target} 1 | cut -d  'x' -f2);
+	ovs_3=$(PrintHeader ${target} 1 | cut -d  'x' -f3);
+	
+	a=$( bc -l <<<"8*$ovs_1 " )
+	b=$( bc -l <<<"8*$ovs_2 " )
+	c=$( bc -l <<<"8*$ovs_3 " )
+	#a=$(( 8*ovs_1 ));
+	#b=$(( 8*ovs_2 ));
+	#c=$(( 8*ovs_3 ));
+	
+	if [[ ! -f ${input} ]];then
+			#ResampleImageBySpacing 3 ${image} ${input} .2 .2 .2 0;
+			ResampleImageBySpacing 3 ${image} ${input} ${a} ${b} ${c} 0;
+	fi
+	
+	ds_target=${tmp_work}${target_name%.nii???}_x8_downsampled.nii.gz;
+	if [[ ! -f ${ds_target} ]];then
+			#ResampleImageBySpacing 3 ${image} ${input} .2 .2 .2 0;
+			ResampleImageBySpacing 3 ${target_folder}/${target_name} ${ds_target} ${a} ${b} ${c} 0;
+	fi
+fi
+
+for out_code in ${out_codes[@]}; do
+        out_image=${output_dir}/${file_name%.nii???}_${out_code}.nii.gz;
+        if [[ ! -f ${out_image} ]]; then
+                bash /mnt/clustertmp/common/rja20_dev//matlab_execs_for_SAMBA//img_transform_executable/run_img_transform_exec.sh /mnt/clustertmp/common/rja20_dev//MATLAB2015b_runtime/v90 ${input} ${in_code} ${out_code} ${out_image};
+        fi
+done
